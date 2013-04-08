f67c575c8778271487ff9f3e31bb830cf3260b56
Matt Tierney <tierney@cs.nyu.edu>
2013-04-08 16:32:23 -0400

    Tweet code slightly cleaner.

--- analysis.py
+++ analysis.py
@@ -4,31 +4,33 @@ import sys
 import json
 import bz2
 
+
+def ProcessTweet(tweet):
+  # Searches for retweeted status in tweet dict's keys.
+  if 'retweeted_status' in tweet.keys():
+    print tweet['retweeted_status']['user']['screen_name'],
+    print ',',
+    print tweet['retweeted_status']['user']['followers_count'],
+    print ',',
+    print tweet['retweeted_status']['text'].encode('utf-8').replace('\n',' '),
+    print
+
+
 def main(argv):
+  # First argument is the data file to read.
   fnout = argv[1]
+
+  # Figure out which function should open the file.
   fopen = open
   if fnout.endswith('.bz2'):
     fopen = bz2.BZ2File
 
   with fopen(fnout, 'rb') as fh:
+    # Process every line as its own JSON tweet.
     for line in fh:
       loaded = json.loads(line)
-      to_print = []
-      if 'retweeted_status' in loaded.keys():
-        # print json.dumps(loaded['retweeted_status']['user'], indent = 2)
-        # to_print.append(loaded['retweeted_status']['id'])
-        print loaded['retweeted_status']['user']['screen_name'],
-        print ',',
-        print loaded['retweeted_status']['user']['followers_count'],
-        print ',',
-        print loaded['retweeted_status']['text'].encode('utf-8').replace('\n',' '),
-        print
+      ProcessTweet(loaded)
 
-      #print loaded.keys()
-      #print loaded['retweeted']
-      #print loaded['retweet_count']
-      # print json.dumps(loaded, indent = 2)
-      # print loaded['entities']
 
 if __name__=='__main__':
   main(sys.argv)
