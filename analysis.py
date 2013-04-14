a2399e4a4624ff191183257e20cae298619e55bb
Paul Torres <pat227@nyu.edu>
2013-04-14 13:05:22 -0400

    Almost got lunch...

--- analysis.py
+++ analysis.py
@@ -23,13 +23,13 @@ def exists_anywhere(rid,d):
       max2 = len(l)
       for y in range(max2):
         item = l[y]
-        if item = rid:
-          print "\nFound a continuation in a retweet chain from id: " rid
+        if item == rid:
+          print "===Found a continuation in a retweet chain from id: " + str(rid) + "==="
           #since we need to return to this location again to update it, 
           #return a tuple to save time and spare us a second search
           #tell us key under which id exists, which list, which element of that list
           return (True, ids, x, y)
-  print "\nDidn't find any continuation for id:" rid
+  #print "Didn't find any continuation for id:" + str(rid)
   return (False, -1,-1,-1)
 
 #check what is the mapping of id and update accordingly
@@ -42,6 +42,7 @@ def update_key_mapping(id,rid,d):
 #given a location, go to that location and insert the retweet id either at end of an existing list
 #if possible, else as the terminal of a new list, reflecting a fork (branch) in a retweet flow
 def update_list_mapping(id,rid,d, locationTuple):
+  print "\tupdate_list_mapping..."
   key = locationTuple[1]
   nthlist = locationTuple[2]
   nthelement = locationTuple[3]
@@ -49,7 +50,7 @@ def update_list_mapping(id,rid,d, locationTuple):
   listlist = d[key]
   thelist = listlist[nthlist]
   #if nthelement is also last, just append to end of list
-  if locationTuple[3] = len(thelist):
+  if locationTuple[3] == len(thelist):
     thelist.append(id)
   else:
   #else have a fork; have to duplicate all elements up to this element, then append, then append again
@@ -62,6 +63,7 @@ def update_list_mapping(id,rid,d, locationTuple):
 #any source to tell me--that this isn't possible, ie. that retweets are always nested but only 1 level
 #at maximum
 def update_pushkey_mapping(id,rid,d):
+  print "\tupdate_pushkey_mapping..."
   #in this case a retweet rid is being retweeted in a more recent tweet with id, but our dictionary
   #has id as a key (a starting point). So the key must be replaced by rid and id prepended to 
   #all the entries in the list of id lists, like so:
@@ -73,6 +75,7 @@ def update_pushkey_mapping(id,rid,d):
   d[rid] = fixedlists
 
 def update_dictionary(id, rid, d):
+  #print "Updating the dictionary..."
   #case I: if d has id -> id list list mapping, update the mapping
   if rid in d:
     update_key_mapping(id,rid,d)
@@ -90,7 +93,7 @@ def update_dictionary(id, rid, d):
     #case III: if d has no id -> id list list mapping and it doesn't appear anywhere else
     #then add it as a new key -> id list list mapping with rid as the only element in the only list
     #to which the key maps
-    d[id]=([[rid]])
+    d[rid]=([[id]])
 
 
 
@@ -101,13 +104,13 @@ def ProcessTweet(tweet, diction):
     id = tweet['id']
     #rid is the unique id of the tweet that is being retweeted
     rid = tweet['retweeted_status']['id']
-    print "\n---PROCESSING A RETWEET ---\n"
-    print " ID: "
-    print id
-    print " TIME: "
-    print tweet['retweeted_status']['created_at']
-    print ', RETWEET ID:',
-    print rid
+#    print "\n---PROCESSING A RETWEET ---"
+#    print " ID: "
+#    print id
+#    print " TIME: "
+#    print tweet['retweeted_status']['created_at']
+#    print ', RETWEET ID:',
+#    print rid
     update_dictionary(id, rid, diction)
 
 def printDictionary(f, d):
@@ -115,10 +118,11 @@ def printDictionary(f, d):
   keys = d.keys()
   for k in keys:
     mapping = d[k]
+    outfile.write("\n")
     outfile.write(str(k))
     outfile.write(" ->\t")
     for listchain in mapping:
-      for elem in listchan:
+      for elem in listchain:
         outfile.write(str(elem))
         outfile.write(", ")
     outfile.write("\n\t\t | ")
@@ -142,12 +146,12 @@ def main(argv):
   with fopen(fnout, 'rb') as fh:
     # Process every line as its own JSON tweet.
     for line in fh:
-      loaded = json.loads(line)
       try:
-        ProcessTweet(loaded)
+        loaded = json.loads(line)
+        ProcessTweet(loaded, d)
       except KeyboardInterrupt:
         printDictionary(outfile,d)
         raise KeyboardInterrupt
-
+    printDictionary(outfile,d)
 if __name__=='__main__':
   main(sys.argv)
