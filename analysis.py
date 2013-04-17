ce945c5befab06f474420b67545e13ac675dec37
paul <paul@lively.westell.com>
2013-04-17 18:49:52 -0400

    Getting back to moving forward after found a bug in prior code.

--- analysis.py
+++ analysis.py
@@ -3,112 +3,82 @@
 import sys
 import json
 import bz2
+import datetime
+#Untested code
+#start using this soon rather than just tracking plain old tweet IDs
+class Node:
+  text
+  dateANDtime
 
-#in retrospect, this task is like (not binary but b-)tree reconstruction given 
-#many nodes with pointers to children (retweet ids and ids). We could end up 
-#with many trees, of various degrees of bushiness and depth, and we have no idea 
-#which elements are the roots of each tree. But perhaps that approach would be 
-#better than lists of lists, as below, which represent paths to leaves from 
-#higher nodes that may or may not be roots. 
+  def stringMonth2int(s):
+    if s == "Jan": return 1
+    if s == "Feb": return 2
+    if s == "Mar": return 3
+    if s == "Apr": return 4
+    if s == "May": return 5
+    if s == "Jun": return 6
+    if s == "Jul": return 7
+    if s == "Aug": return 8
+    if s == "Sep": return 9
+    if s == "Oct": return 10
+    if s == "Nov": return 11
+    if s == "Dec": return 12
+    return 13 #this will cause a failure
 
-#possibly super slow dumb way of doing this...
-def exists_anywhere(rid,d):
-  #double iterate over id list list for every key to find the id
-  #anywhere it might exist (we already know it is not a key)...super slow operation
-  for ids in d:
-    listlist = d[ids]
-    max = len(listlist)
-    for x in range(max):
-      l = listlist[x]
-      max2 = len(l)
-      for y in range(max2):
-        item = l[y]
-        if item == rid:
-          print "===Found a continuation in a retweet chain from id: " + str(rid) + "to id: " + "==="
-          #since we need to return to this location again to update it, 
-          #return a tuple to save time and spare us a second search
-          #tell us key under which id exists, which list, which element of that list
-          return (True, ids, x, y)
-  #print "Didn't find any continuation for id:" + str(rid)
-  return (False, -1,-1,-1)
+  def setTime(s):
+    #Mon Mar 11 15:30:27 +0000 2013 -> datetime object
+    slist = s.split(" ")
+    #year, month, day[, hour[, minute[, second[, microsecond[, tzinfo]]]]]
+    timelist = slist[3].split(":")
+    dateANDtime = datetime.datetime.datetime(int(slist[5]), stringMonth2int(slist[1]), int(slist[2]), int(timelist[0]), int(timelist[1]), int(timelist[2]))
+    
+    def setText(s): 
+      text = s
 
-#check what is the mapping of id and update accordingly
-def update_key_mapping(id,rid,d):
-  #already know rid is a key in d
-  listoflists = d[rid]
-  #id must be appended to mapping of rid as single element in its own list
-  listoflists.append([[id]])
-        
-#given a location, go to that location and insert the retweet id either at end of an existing list
-#if possible, else as the terminal of a new list, reflecting a fork (branch) in a retweet flow
-def update_list_mapping(id,rid,d, locationTuple):
-  print "\tupdate_list_mapping..."
-  key = locationTuple[1]
-  nthlist = locationTuple[2]
-  nthelement = locationTuple[3]
-  #go to the location
-  listlist = d[key]
-  thelist = listlist[nthlist]
-  #if nthelement is also last, just append to end of list
-  if locationTuple[3] == len(thelist):
-    thelist.append(id)
-  else:
-  #else have a fork; have to duplicate all elements up to this element, then append, then append again
-    newflow = thelist[0:(locationTuple[3])]
-    newflow.append(id)
-    listlist.append(newflow)
+class Driver:
 
-#if a key in the dictionary turns out to be itself a retweet and we now have the prior tweet
-#this might actully be unneccessary but not seen enough twitter data to convince myself--nor found
-#any source to tell me--that this isn't possible, ie. that retweets are always nested but only 1 level
-#at maximum
-def update_pushkey_mapping(id,rid,d):
-  print "\tupdate_pushkey_mapping..."
-  #in this case a retweet rid is being retweeted in a more recent tweet with id, but our dictionary
-  #has id as a key (a starting point). So the key must be replaced by rid and id prepended to 
-  #all the entries in the list of id lists, like so:
-  lists = d[id]
-  fixedlists = []
-  for l in lists:
-    fixedlists.append([id] + l)
-  del d[id]
-  d[rid] = fixedlists
+  #id -> set of id  map, like a set of production rules
+  #id -> [id,id...]
+  #id -> [id]
+  #...
+  id2sets = dict()
+  #set of all entries;determining if an id is a root or not requires we either 
+  #search all mappings to determine if we've ever seen it, or maintain some 
+  #way of answering that question
+  universe = set()
+  #set of roots
+  rootset = set()
 
-def update_dictionary(id, rid, d):
-  #print "Updating the dictionary..."
-  #case I: if d has id -> id list list mapping, update the mapping
-  if rid in d:
-    update_key_mapping(id,rid,d)
-    return
-  if id in d:
-    update_pushkey_mapping(id,rid,d)
-    return
-  #else
-  #case II & III: if d has no id -> id list list mapping, check for existence of id anywhere else
-  locationTuple = exists_anywhere(id,d)
-  if locationTuple[0]:
-    #case II: if id exists anywhere, update the mapping, forking if need be
-    update_list_mapping(id,rid,d, locationTuple)
-  else:
-    #case III: if d has no id -> id list list mapping and it doesn't appear anywhere else
-    #then add it as a new key -> id list list mapping with rid as the only element in the only list
-    #to which the key maps
-    d[rid]=([[id]])
+  def update_mappings(id,rid):
+    if rid in id2sets:
+      #add to set (branch in flow), & add it to set of all know id's (so we know
+      #it is not a root in future)
+      id2sets[rid] = id2sets[rid].add(id)
+      universe.add(id)
+    else:
+      #add to keys and start new mapping (continuation of a flow), and make note
+      #of it being a new root & add it to set of all known id's (next 3 lines
+      #fail if attempted as 1 line)
+      temps = id2sets[rid]
+      temps.add(id)
+      id2sets[rid] = temps
+      #check for pre-existence in any set and add to set of roots if it is new
+      if rid not in universe:
+        rootset.add(rid)
+        universe.add(rid)
+      universe.add(id)
+
+  def ProcessTweet(tweet, diction):
+    if 'retweeted_status' in tweet.keys():
+      #id is is unique id of this tweet, a retweet of a prior tweet
+      id = tweet['id']
+      #rid is the unique id of the tweet that is being retweeted
+      rid = tweet['retweeted_status']['id']
+      update_mappings(id, rid, diction)
 
 
-
-def ProcessTweet(tweet, diction):
-  # Searches for retweeted status in tweet dict's keys.
-  if 'retweeted_status' in tweet.keys():
-    #id is is unique id of this tweet, a retweet of a prior tweet
-    id = tweet['id']
-    #rid is the unique id of the tweet that is being retweeted
-    rid = tweet['retweeted_status']['id']
-    update_dictionary(id, rid, diction)
-
-#somewhat pretty print the dictionary--had a bug, actually had lots of star 
-#topologies NOT chains in prior run
-def printDictionary(f, d):
+  #somewhat pretty print the dictionary--fixed bug from prior file
+  def printDictionary(f, d):
   outfile = open(f,'w')
   keys = d.keys()
   for k in keys:
@@ -118,7 +88,7 @@ def printDictionary(f, d):
     outfile.write(" ->\t")
     max = len(mapping)
     for x in range(max):
-      if x > 0: outfile.write("\n\t\t | ")
+      if x > 0: print "\n\t\t | "
       listchain = mapping[x]
       for elem in listchain:
         outfile.write(str(elem))
@@ -126,35 +96,26 @@ def printDictionary(f, d):
   outfile.flush()
   outfile.close()
 
-#to screen version of above function
-def printDictionary_toScreen(d):
-  keys = d.keys()
-  for k in keys:
-    mapping = d[k]
-    print("\n")
-    print(str(k))
-    print(" ->\t")
-    max = len(mapping)
-    for x in range(max):
-      if x > 0: print " | "
-      listchain = mapping[x]
-      for elem in listchain:
-        print(str(elem))
-        print(", ")
+  def printDictionary_toScreen(d):
+    keys = d.keys()
+    for k in keys:
+      mapping = d[k]
+      print("\n")
+      print(str(k))
+      print(" ->\t")
+      max = len(mapping)
+      for x in range(max):
+        if x > 0: print " | "
+        listchain = mapping[x]
+        for elem in listchain:
+          print(str(elem))
+          print(", ")
 
-
-def main(argv):
-  #use a dictionary to map from IDs to lists of lists of retweet IDs
-  d = dict()
-  # First argument is the data file to read.
-  fnout = argv[1]
-  # Second argument is the data file to which to write upon completion or interruption
-  outfile = argv[2]
+  def process_TweetsFromFile(fnout, outfile):
   # Figure out which function should open the file.
   fopen = open
   if fnout.endswith('.bz2'):
     fopen = bz2.BZ2File
-
 
   with fopen(fnout, 'rb') as fh:
     # Process every line as its own JSON tweet.
@@ -162,11 +123,30 @@ def main(argv):
       try:
         loaded = json.loads(line)
         ProcessTweet(loaded, d)
-      #print the dictionary to the out file even if we interrupt further progress
+      #print the dictionary to out file even if we interrupt further progress
       except KeyboardInterrupt:
         printDictionary(outfile,d)
-        exit()
         raise KeyboardInterrupt
     printDictionary(outfile,d)
+
+  #incase have to 2 go to file to get it, but this would be slow
+  def getTweetText(id, f):
+    for line in f:
+      if 'id' in tweet.keys():
+        tid = tweet['id']
+        if id == tid:
+          if 'text' in tweet.keys():
+            return tweet['text']
+
+
+  
+def main(argv):
+  driver = Driver()
+  #First argument is the data file to read.
+  #Second argument is the data file to which to write upon completion or 
+  #interruption
+  process_TweetsFromFile(argv[1],argv[2])
+
+
 if __name__=='__main__':
   main(sys.argv)
